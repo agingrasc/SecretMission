460d9caaf9b6febb99fdbe0b412fb228d2dbc208
Jenifael Gingras-Courchesne <agingrasc@gmail.com>
2018-01-28 16:30:13 -0500

    Extraction des classes dans des fichiers separes.

---
+++ SecretMission/ATM.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecretMission
+{
+    public class ATM
+    {
+        private static readonly List<Account> AccountsList;
+        private Account _curAc;         
+
+        static ATM()
+        {
+            AccountsList = new List<Account>();
+        }
+
+        public void CreateAccount()
+        {
+            try
+            {
+                var randomGenerator = new Random();
+                Console.WriteLine(@"Choose a PIN: ");
+                var acc = new Account(int.Parse(Console.ReadLine()), randomGenerator.Next(1, 10000));
+                acc.GenerateAccount();
+                AccountsList.Add(acc);
+                _curAc = acc;
+                Console.WriteLine($@"Your account number is : {acc.AccountNo}");
+            }
+            catch (Exception)
+            {
+                //Nothing
+            }
+        }
+
+        public void AccountInfo()
+        {
+            //var accountExists = ValidateAccount();
+            //Validating that the account exists
+            Console.WriteLine(@"Enter your account number: ");
+            var accountNo = int.Parse(Console.ReadLine());
+            Console.WriteLine(@"Enter your PIN: ");
+            var pinNo = int.Parse(Console.ReadLine());
+
+            var accountExists = AccountsList.Exists(x => x.AccountNo == accountNo && x.PinNo == pinNo);
+
+            Console.WriteLine(
+                accountExists
+                    ? $@"{Environment.NewLine}First name: {_curAc.FirstName}{Environment.NewLine}Last name: {_curAc.LastName}{Environment.NewLine}Phone number: {_curAc.PhoneNo}{Environment.NewLine}Date of birth: { _curAc.DateOfBirth}{Environment.NewLine}": @"Account number and PIN do not match.");
+        }
+
+        public void Deposit()
+        {
+            //TODO: this will work some day
+            var validAccount = ValidateAccount();
+
+            if (!validAccount)
+            {
+                Console.WriteLine(@"Account number and PIN do not match.");
+                return;
+            }
+
+            Console.WriteLine($@"Your account balance is: {_curAc.Balance}");
+            Console.WriteLine(@"Enter the amount you wish to deposit");
+            var amount = double.Parse(Console.ReadLine());
+            _curAc.Balance += amount;
+        }
+
+        public void Withdraw()
+        {
+            //Validating that the account exists
+            Console.WriteLine(@"Enter your account number: ");
+            var accountNo = int.Parse(Console.ReadLine());
+            Console.WriteLine(@"Enter your PIN: ");
+            var pinNo = int.Parse(Console.ReadLine());
+
+            var accountExists = AccountsList.Exists(x => x.AccountNo == accountNo || x.PinNo == pinNo);
+
+            if (accountExists)
+            {
+                var account = AccountsList.Find(x => x.AccountNo == accountNo && x.PinNo == pinNo);
+                _curAc = account;
+            }
+
+            if (!accountExists)
+            {
+                Console.WriteLine(@"Account number and PIN do not match.");
+                return;
+            }
+
+            Console.WriteLine($@"Your account balance is: {_curAc.Balance}");
+            Console.WriteLine(@"Enter the amount you wish to withdraw");
+            var amount = double.Parse(Console.ReadLine());
+            if (_curAc.Balance < amount)
+            {
+                Console.WriteLine(@"Your current balance is less than the amount you wish to withdraw.");
+                return;
+            }
+            _curAc.Balance -= amount;
+        }
+
+        private bool ValidateAccount()
+        {
+            return false;
+            //TODO: maybe write some code here because it is duplicated in more than one method
+        }
+    }
+}
---
+++ SecretMission/Account.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace SecretMission
+{
+    public interface IAccount
+    {
+        //TODO: maybe some day this interface will be used to create other types of accounts... or not
+        void GenerateAccount();
+    }
+
+    public class Account : IAccount
+    {
+        
+
+        public Account(int pinNo, int accountNo)
+        {
+            _pinNo = 1234567890;
+            _accountNo = accountNo;
+            _balance = 0;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception();
+                _firstName = value;
+            }
+        }
+
+        string _firstName, _lastName, _dateOfBirth, _phoneNo;
+        double _balance;
+        int _pinNo, _accountNo;
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception();
+                _lastName = value;
+            }
+        }
+        public string DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception();
+                _dateOfBirth = value;
+            }
+        }
+        public string PhoneNo
+        {
+            get { return _phoneNo; }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new Exception();
+                _phoneNo = value;
+            }
+        }
+
+        public int AccountNo
+        {
+            get => _accountNo;
+            set
+            {
+                if (_accountNo < 0) throw new Exception();
+                _accountNo = value;
+            }
+        }
+
+        public int PinNo
+        {
+            get => _pinNo;
+            set
+            {
+                if (_pinNo >= 0)
+                    _pinNo = value;
+                else
+                    throw new Exception();
+            }
+        }
+
+        public double Balance
+        {
+            get => _balance;
+            set
+            {
+                if (double.IsNaN(value)) throw new Exception();
+                _balance = value;
+            }
+        }
+
+        public void GenerateAccount()
+        {
+            Console.WriteLine(@"Enter your first name: ");
+            _firstName = Console.ReadLine();
+            Console.WriteLine(@"Enter your last name: ");
+            _lastName = Console.ReadLine();
+            Console.WriteLine(@"Enter your phone number: ");
+            _phoneNo = Console.ReadLine();
+            Console.WriteLine(@"Enter your date of birth: ");
+            _dateOfBirth = Console.ReadLine();
+        }
+    }
+}
