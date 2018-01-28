d5403ca897d7b6a2776b715a3bd21fa92c3554a6
Jenifael Gingras-Courchesne <agingrasc@gmail.com>
2018-01-28 18:51:05 -0500

    Ajout interface ReaderWriter et implementation ConsoleWrapper

---
+++ SecretMission/ConsoleWrapper.cs
@@ -0,0 +1,29 @@
+﻿namespace SecretMission
+{
+    public interface IWriter
+    {
+        void WriteLine(string line);
+    }
+
+    public interface IReader
+    {
+        string ReadLine();
+    }
+
+    public interface IReaderWriter : IWriter, IReader
+    {
+    }
+
+    public class ConsoleWrapper: IReaderWriter
+    {
+        public void WriteLine(string line)
+        {
+            System.Console.WriteLine(line);
+        }
+
+        public string ReadLine()
+        {
+            return System.Console.ReadLine();
+        }
+    }
+}
