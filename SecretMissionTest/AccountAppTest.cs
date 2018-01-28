a31aa23777d784d3818cfe410afb2b079536846e
Jenifael Gingras-Courchesne <agingrasc@gmail.com>
2018-01-28 16:11:26 -0500

    Ajout d'un projet de tests.

---
+++ SecretMissionTest/AccountAppTest.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace SecretMissionTest
+{
+    [TestFixture]
+    public class SecretMissionTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
