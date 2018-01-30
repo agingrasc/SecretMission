5e906c3c9e1132be793ca4453e97cd19b66bff75
Jenifael Gingras-Courchesn <agingrasc@gmail.com>
2018-01-29 19:18:42 -0500

    Refactor extract function dans le Main
    * Separation de la logique d'affichage
    * Separation de la logique de saisie
    * Separation de la logique du menu
    * Ajout d'une option pour quitter l'application
    * Retrait du deuxieme switch imbrique, un seul niveau de menu

--- SecretMission/AccountApp.cs
+++ SecretMission/AccountApp.cs
@@ -4,51 +4,67 @@ namespace SecretMission
 {
     public class Program
     {
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("Menu");
+            Console.WriteLine("1.Create Account");
+            Console.WriteLine("2.Deposit");
+            Console.WriteLine("3.Withdraw");
+            Console.WriteLine("4.Account info");
+            Console.WriteLine("5.Exit");
+            Console.Write("Please enter your selection: ");
+        }
+
+        private static int GetMenuChoice()
+        {
+            var menuChoice = -1;
+            try
+            {
+                menuChoice = int.Parse(Console.ReadLine());
+            }
+            catch (ArgumentNullException)
+            {
+            }
+
+            return menuChoice;
+        }
+
+        private static bool ExecuteMenuSelection(int menuChoice, Atm atm)
+        {
+            switch (menuChoice)
+            {
+                case 1:
+                    atm.CreateAccount();
+                    break;
+                case 2:
+                    atm.Deposit();
+                    break;
+                case 3:
+                    atm.Withdraw();
+                    break;
+                case 4:
+                    atm.AccountInfo();
+                    break;
+                case 5:
+                    return true;
+                default:
+                    Console.WriteLine(@"Invalid selection!");
+                    break;
+            }
+
+            return false;
+        }
+
         static void Main(string[] args)
         {
             var accountFactory = new AccountFactory(new RandomAccountNumberGenerator());
             var atm = new Atm(new ConsoleWrapper(), accountFactory);
-            while (true)
+            var exit = false;
+            while (!exit)
             {
-                Console.WriteLine(@"Menu");
-                Console.WriteLine(@"1.Create Account");
-                Console.WriteLine(@"2.ATM");
-                Console.WriteLine(@"3.Account info");
-                Console.Write(@"Please enter your selection: ");
-                var menuChoice = int.Parse(Console.ReadLine());
-
-                //Wow, look at that switch case. My eyes are burning. Switchception.
-                switch (menuChoice)
-                {
-                    case 1:                       
-                        atm.CreateAccount();
-                        break;
-                    case 2:
-                        //Console.WriteLine(@"1.Deposit Or Withdraw");
-                        Console.WriteLine(@"1.Deposit");
-                        Console.WriteLine(@"2.Withdraw");
-                        Console.Write(@"Please enter your selection: ");
-                        var atmMenuChoice = int.Parse(Console.ReadLine());
-                        switch (atmMenuChoice)
-                        {
-                            case 1:
-                                atm.Deposit();
-                                break;
-                            case 2:
-                                atm.Withdraw();
-                                break;
-                            default:
-                                Console.WriteLine(@"Invalid selection!");
-                                break;
-                        }
-                        break;
-                    case 3:
-                        atm.AccountInfo();
-                        break;
-                    default:
-                        Console.WriteLine(@"Invalid selection!");
-                        break;
-                }
+                DisplayMenu();
+                var menuChoice = GetMenuChoice();
+                exit = ExecuteMenuSelection(menuChoice, atm);
             }
         }
     }
